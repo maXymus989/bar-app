16952e8bb185e8c935d302d006cb750d595c6289
maxymus989 <989m66@gmail.com>
2025-05-24 23:29:43 +0300

    Alert Dialog design updated

--- Components/AlertDialog.jsx
+++ Components/AlertDialog.jsx
@@ -1,5 +1,7 @@
-import { Text } from "react-native";
+import { useContext } from "react";
+import { Text, StyleSheet } from "react-native";
 import { Dialog } from "@rneui/base";
+import { ColorThemeContext } from "../app/index";
 
 const AlertDialog = ({
   visible,
@@ -7,16 +9,68 @@ const AlertDialog = ({
   alertText,
   alertTitle = "Попередження",
 }) => {
+  const ColorPalette = useContext(ColorThemeContext);
+
   return (
     <Dialog
       isVisible={visible}
       onBackdropPress={setVisible}
-      overlayStyle={{ backgroundColor: "white" }}
+      overlayStyle={[
+        styles.window,
+        { backgroundColor: ColorPalette.main.buttons_modalBackground },
+      ]}
     >
-      <Dialog.Title title={alertTitle} />
-      <Text>{alertText}</Text>
+      <Text
+        style={[
+          styles.text,
+          {
+            fontSize: 24,
+            fontFamily: "KyivTypeTitling-Bold",
+            color: ColorPalette.main.darkText,
+          },
+        ]}
+      >
+        {alertTitle}
+      </Text>
+      <Text
+        style={[
+          styles.text,
+          {
+            color: ColorPalette.main.darkText,
+          },
+        ]}
+      >
+        {alertText}
+      </Text>
     </Dialog>
   );
 };
 
 export default AlertDialog;
+
+const styles = StyleSheet.create({
+  window: {
+    alignItems: "center",
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 18,
+    width: "90%",
+    marginVertical: 20,
+    fontFamily: "KyivTypeSerif-Heavy",
+  },
+  buttonContainer: {
+    marginVertical: "2%",
+    marginHorizontal: 20,
+    width: 80,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.0,
+    elevation: 24,
+  },
+});
