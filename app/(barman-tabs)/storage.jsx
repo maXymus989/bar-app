06db56747138dacd2ac0011ec2a6e9f17da421f8
BohdanFL <robot228rb@gmail.com>
2025-08-02 00:04:48 +0300

    feat: displaying the availability of goods in the warehouse

--- app/(barman-tabs)/storage.jsx
+++ app/(barman-tabs)/storage.jsx
@@ -1,127 +1,129 @@
-import { useContext, useState, useEffect, useCallback } from "react";
+import { useContext, useState, useEffect, useCallback } from 'react';
 import {
-  ScrollView,
-  StyleSheet,
-  View,
-  ActivityIndicator,
-  RefreshControl,
-} from "react-native";
-import { FAB } from "@rneui/base";
-import { ColorThemeContext } from "../index";
-import StorageItem from "../../Components/StorageItem";
-import StorageItemDialog from "../../Components/StorageItemDialog";
-import useBarStore from "../../state";
+    ScrollView,
+    StyleSheet,
+    View,
+    ActivityIndicator,
+    RefreshControl,
+} from 'react-native';
+import { FAB } from '@rneui/base';
+import { ColorThemeContext } from '../index';
+import StorageItem from '../../Components/StorageItem';
+import StorageItemDialog from '../../Components/StorageItemDialog';
+import useBarStore from '../../state';
 
 const Storage = () => {
-  const ColorPalette = useContext(ColorThemeContext);
-  const [addItemDialogVisible, setAddItemDialogVisible] = useState(false);
-  const [updateItemDialogVisible, setUpdateItemDialogVisible] = useState(false);
-  const [currentItemId, setCurrentItemId] = useState("");
+    const ColorPalette = useContext(ColorThemeContext);
+    const [addItemDialogVisible, setAddItemDialogVisible] = useState(false);
+    const [updateItemDialogVisible, setUpdateItemDialogVisible] =
+        useState(false);
+    const [currentItemId, setCurrentItemId] = useState('');
 
-  const {
-    storage,
-    addStorageItem,
-    updateStorageItem,
-    removeStorageItem,
-    isStorageLoaded,
-    fetchStorageData,
-  } = useBarStore();
+    const {
+        storage,
+        addStorageItem,
+        updateStorageItem,
+        removeStorageItem,
+        isStorageLoaded,
+        fetchStorageData,
+    } = useBarStore();
 
-  useEffect(() => {
-    if (!isStorageLoaded) {
-      fetchStorageData();
-    }
-  }, [isStorageLoaded]);
-
-  const openStorageItemDialog = (storageItemId) => {
-    setCurrentItemId(storageItemId);
-    setUpdateItemDialogVisible(true);
-  };
-
-  const [refreshing, setRefreshing] = useState(false);
-
-  const onRefresh = useCallback(() => {
-    setRefreshing(true);
-    setTimeout(() => {
-      setRefreshing(false);
-    }, 100);
-    fetchStorageData();
-  }, []);
-
-  return (
-    <View
-      style={{
-        backgroundColor: ColorPalette.main.background_modalButtons,
-        flex: 1,
-      }}
-    >
-      <ScrollView
-        contentContainerStyle={styles.window}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+    useEffect(() => {
+        if (!isStorageLoaded) {
+            fetchStorageData();
         }
-      >
-        {!isStorageLoaded ? (
-          <ActivityIndicator size="large" color={"white"} />
-        ) : (
-          storage.map((storageItem, key) => (
-            <StorageItem
-              storageItemObj={storageItem}
-              key={key}
-              onDeleteMenuItem={removeStorageItem}
-              onStorageItemDialog={openStorageItemDialog}
+    }, [isStorageLoaded]);
+
+    const openStorageItemDialog = (storageItemId) => {
+        setCurrentItemId(storageItemId);
+        setUpdateItemDialogVisible(true);
+    };
+
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        setTimeout(() => {
+            setRefreshing(false);
+        }, 100);
+        fetchStorageData();
+    }, []);
+
+    return (
+        <View
+            style={{
+                backgroundColor: ColorPalette.main.background_modalButtons,
+                flex: 1,
+            }}>
+            <ScrollView
+                contentContainerStyle={styles.window}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }>
+                {!isStorageLoaded ? (
+                    <ActivityIndicator size="large" color={'white'} />
+                ) : (
+                    storage.map((storageItem, key) => (
+                        <StorageItem
+                            storageItemObj={storageItem}
+                            key={key}
+                            onDeleteMenuItem={removeStorageItem}
+                            onStorageItemDialog={openStorageItemDialog}
+                        />
+                    ))
+                )}
+            </ScrollView>
+            <FAB
+                // visible={true}
+                title={'+'}
+                color={ColorPalette.main.buttons_modalBackground}
+                style={styles.FABStyle}
+                titleStyle={{
+                    color: ColorPalette.main.darkText,
+                    fontSize: 32,
+                    fontFamily: 'KyivTypeSerif-Heavy',
+                    paddingVertical: 0,
+                }}
+                buttonStyle={{ height: 64 }}
+                onPress={() => setAddItemDialogVisible(true)}
             />
-          ))
-        )}
-      </ScrollView>
-      <FAB
-        visible={true}
-        title={"+"}
-        color={ColorPalette.main.buttons_modalBackground}
-        style={styles.FABStyle}
-        titleStyle={{
-          color: ColorPalette.main.darkText,
-          fontSize: 32,
-          fontFamily: "KyivTypeSerif-Heavy",
-          paddingVertical: 0,
-        }}
-        buttonStyle={{ height: 64 }}
-        onPress={() => setAddItemDialogVisible(true)}
-      />
 
-      {addItemDialogVisible && (
-        <StorageItemDialog
-          isVisible={addItemDialogVisible}
-          setIsVisible={setAddItemDialogVisible}
-          onAddStorageItem={addStorageItem}
-        />
-      )}
+            {addItemDialogVisible && (
+                <StorageItemDialog
+                    isVisible={addItemDialogVisible}
+                    setIsVisible={setAddItemDialogVisible}
+                    onAddStorageItem={addStorageItem}
+                />
+            )}
 
-      {updateItemDialogVisible && (
-        <StorageItemDialog
-          isVisible={updateItemDialogVisible}
-          setIsVisible={setUpdateItemDialogVisible}
-          isNew={false}
-          storageItem={storage.find(
-            (storageItem) => storageItem.id === currentItemId
-          )}
-          onUpdateStorageItem={updateStorageItem}
-          onRemoveStorageItem={removeStorageItem}
-        />
-      )}
-    </View>
-  );
+            {updateItemDialogVisible && (
+                <StorageItemDialog
+                    isVisible={updateItemDialogVisible}
+                    setIsVisible={setUpdateItemDialogVisible}
+                    isNew={false}
+                    storageItem={storage.find(
+                        (storageItem) => storageItem.id === currentItemId
+                    )}
+                    onUpdateStorageItem={updateStorageItem}
+                    onRemoveStorageItem={removeStorageItem}
+                />
+            )}
+        </View>
+    );
 };
 
 export default Storage;
 
 const styles = StyleSheet.create({
-  window: {
-    alignItems: "center",
-  },
-  FABStyle: {
-    position: "absolute",
-    bottom: 20,
-    right: 20,
-  },
+    window: {
+        alignItems: 'center',
+    },
+    FABStyle: {
+        position: 'absolute',
+        bottom: 20,
+        right: 20,
+    },
 });
--- state/index.js
+++ state/index.js
@@ -1,299 +1,319 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 import {
-  collection,
-  getDocs,
-  doc,
-  setDoc,
-  updateDoc,
-  deleteDoc,
-  getDoc,
-} from "firebase/firestore";
+    collection,
+    getDocs,
+    doc,
+    setDoc,
+    updateDoc,
+    deleteDoc,
+    getDoc,
+} from 'firebase/firestore';
 import {
-  ref as storageRef,
-  uploadBytes,
-  getDownloadURL,
-  deleteObject,
-} from "firebase/storage";
-import { getAuth } from "firebase/auth";
-import { firestore, storage } from "../Firebase/firebaseConfig";
+    ref as storageRef,
+    uploadBytes,
+    getDownloadURL,
+    deleteObject,
+} from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
+import { firestore, storage } from '../Firebase/firebaseConfig';
 
 const COLLECTIONS = {
-  menu: "menu",
-  orders: "orders",
-  storage: "storage",
+    menu: 'menu',
+    orders: 'orders',
+    storage: 'storage',
 };
 
 const getUserPath = (barmanEmail) => {
-  if (barmanEmail) {
-    return `bars/${barmanEmail.toLowerCase()}`;
-  }
+    if (barmanEmail) {
+        return `bars/${barmanEmail.toLowerCase()}`;
+    }
 
-  const auth = getAuth();
-  const user = auth.currentUser;
-  if (!user) throw new Error("User is not authorized!");
-  return `bars/${user.email.toLowerCase()}`;
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (!user) throw new Error('User is not authorized!');
+    return `bars/${user.email.toLowerCase()}`;
 };
 
 const getCollection = (name, userPath) => {
-  if (!userPath) throw new Error("getCollection: userPath is undefined");
-  return collection(firestore, `${userPath}/${name}`);
+    if (!userPath) throw new Error('getCollection: userPath is undefined');
+    return collection(firestore, `${userPath}/${name}`);
 };
 
 const loadCollection = async (name, userPath) => {
-  const querySnapshot = await getDocs(getCollection(name, userPath));
-  return querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    const querySnapshot = await getDocs(getCollection(name, userPath));
+    return querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
 };
 
 const useBarStore = create((set, get) => ({
-  rooms: [],
-  menu: [],
-  orders: [],
-  storage: [],
-  areRoomsLoaded: false,
-  isMenuLoaded: false,
-  isOrdersLoaded: false,
-  isStorageLoaded: false,
+    rooms: [],
+    menu: [],
+    orders: [],
+    storage: [],
+    areRoomsLoaded: false,
+    isMenuLoaded: false,
+    isOrdersLoaded: false,
+    isStorageLoaded: false,
 
-  guestUsername: "",
-  barmanEmail: "",
+    guestUsername: '',
+    barmanEmail: '',
 
-  setGuest: (guestUsername, barmanUsername) => {
-    console.log(barmanUsername);
-    set({
-      guestUsername,
-      barmanEmail: barmanUsername + "@barapp.com",
-    });
-  },
+    setGuest: (guestUsername, barmanUsername) => {
+        console.log(barmanUsername);
+        set({
+            guestUsername,
+            barmanEmail: barmanUsername + '@barapp.com',
+        });
+    },
 
-  fetchMenuData: async (guestRequest = false) => {
-    try {
-      const { barmanEmail } = get();
-      const menu = await loadCollection(
-        COLLECTIONS.menu,
-        getUserPath(guestRequest ? barmanEmail : "")
-      );
-      set({ menu, isMenuLoaded: true });
-    } catch (e) {
-      console.error("Failed to fetch menu:", e);
-    }
-  },
-
-  fetchOrdersData: async (guestRequest = false) => {
-    try {
-      const { barmanEmail } = get();
-      const orders = await loadCollection(
-        COLLECTIONS.orders,
-        getUserPath(guestRequest ? barmanEmail : "")
-      );
-      set({ orders, isOrdersLoaded: true });
-    } catch (e) {
-      console.error("Failed to fetch orders:", e);
-    }
-  },
-
-  fetchStorageData: async (guestRequest = false) => {
-    try {
-      const { barmanEmail } = get();
-      const storage = await loadCollection(
-        COLLECTIONS.storage,
-        getUserPath(guestRequest ? barmanEmail : "")
-      );
-      set({ storage, isStorageLoaded: true });
-    } catch (e) {
-      console.error("Failed to fetch storage:", e);
-    }
-  },
-
-  fetchRooms: async () => {
-    try {
-      const barsCollection = collection(firestore, "bars");
-      const querySnapshot = await getDocs(barsCollection);
-      set({
-        rooms: querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })),
-        areRoomsLoaded: true,
-      });
-    } catch (e) {
-      console.error("Failed to fetch rooms:", e);
-    }
-  },
-
-  addMenuItem: async (item) => {
-    try {
-      let downloadURL = "";
-      let storagePath = "";
-      if (item.image) {
-        const imageUri = item.image;
-        const response = await fetch(imageUri);
-        const blob = await response.blob();
-
-        const imageName = `${Date.now()}_${Math.random()
-          .toString(36)
-          .substring(7)}.jpg`;
-        storagePath = `${getUserPath("")}/menu_images/${imageName}`;
-        const imageRef = storageRef(storage, storagePath);
-
-        await uploadBytes(imageRef, blob);
-        downloadURL = await getDownloadURL(imageRef);
-      }
-
-      const finalItem = {
-        ...item,
-        image: downloadURL,
-        imagePath: storagePath,
-      };
-
-      const docRef = doc(getCollection(COLLECTIONS.menu, getUserPath("")));
-      await setDoc(docRef, finalItem);
-
-      set((state) => ({
-        menu: [...state.menu, { ...finalItem, id: docRef.id }],
-      }));
-    } catch (error) {
-      console.error(error);
-    }
-  },
-
-  clearAll: () => {
-    set({
-      rooms: [],
-      menu: [],
-      orders: [],
-      storage: [],
-      areRoomsLoaded: false,
-      isMenuLoaded: false,
-      isOrdersLoaded: false,
-      isStorageLoaded: false,
-
-      guestUsername: "",
-      barmanEmail: "",
-    });
-  },
-
-  updateMenuItem: async (id, newData) => {
-    try {
-      const itemRef = doc(
-        getCollection(COLLECTIONS.menu, getUserPath("")),
-        id.toString()
-      );
-      const itemSnap = await getDoc(itemRef);
-      if (!itemSnap.exists()) return;
-
-      const oldData = itemSnap.data();
-      let downloadURL = oldData.image;
-      let storagePath = oldData.imagePath;
-
-      if (newData.image && newData.image.startsWith("file://")) {
-        if (oldData.imagePath) {
-          try {
-            const oldImageRef = storageRef(storage, oldData.imagePath);
-            await deleteObject(oldImageRef);
-          } catch (err) {
-            console.warn("Не вдалося видалити старе зображення:", err);
-          }
+    fetchMenuData: async (guestRequest = false) => {
+        try {
+            const { barmanEmail } = get();
+            const menu = await loadCollection(
+                COLLECTIONS.menu,
+                getUserPath(guestRequest ? barmanEmail : '')
+            );
+            set({ menu, isMenuLoaded: true });
+        } catch (e) {
+            console.error('Failed to fetch menu:', e);
         }
+    },
 
-        const response = await fetch(newData.image);
-        const blob = await response.blob();
+    fetchOrdersData: async (guestRequest = false) => {
+        try {
+            const { barmanEmail } = get();
+            const orders = await loadCollection(
+                COLLECTIONS.orders,
+                getUserPath(guestRequest ? barmanEmail : '')
+            );
+            set({ orders, isOrdersLoaded: true });
+        } catch (e) {
+            console.error('Failed to fetch orders:', e);
+        }
+    },
 
-        const imageName = `${Date.now()}_${Math.random()
-          .toString(36)
-          .substring(7)}.jpg`;
-        storagePath = `${getUserPath("")}/menu_images/${imageName}`;
-        const imageRef = storageRef(storage, storagePath);
+    fetchStorageData: async (guestRequest = false) => {
+        try {
+            const { barmanEmail } = get();
+            const storage = await loadCollection(
+                COLLECTIONS.storage,
+                getUserPath(guestRequest ? barmanEmail : '')
+            );
+            set({ storage, isStorageLoaded: true });
+        } catch (e) {
+            console.error('Failed to fetch storage:', e);
+        }
+    },
 
-        await uploadBytes(imageRef, blob);
-        downloadURL = await getDownloadURL(imageRef);
-      }
+    fetchRooms: async () => {
+        try {
+            const barsCollection = collection(firestore, 'bars');
+            const querySnapshot = await getDocs(barsCollection);
+            set({
+                rooms: querySnapshot.docs.map((doc) => ({
+                    ...doc.data(),
+                    id: doc.id,
+                })),
+                areRoomsLoaded: true,
+            });
+        } catch (e) {
+            console.error('Failed to fetch rooms:', e);
+        }
+    },
 
-      const finalData = {
-        ...newData,
-        image: downloadURL,
-        imagePath: storagePath,
-      };
+    addMenuItem: async (item) => {
+        try {
+            let downloadURL = '';
+            let storagePath = '';
+            if (item.image) {
+                const imageUri = item.image;
+                const response = await fetch(imageUri);
+                const blob = await response.blob();
 
-      await updateDoc(itemRef, finalData);
-      set((state) => ({
-        menu: state.menu.map((item) =>
-          item.id === id ? { ...item, ...finalData } : item
-        ),
-      }));
-    } catch (error) {
-      console.error(error);
-    }
-  },
+                const imageName = `${Date.now()}_${Math.random()
+                    .toString(36)
+                    .substring(7)}.jpg`;
+                storagePath = `${getUserPath('')}/menu_images/${imageName}`;
+                const imageRef = storageRef(storage, storagePath);
 
-  removeMenuItem: async (id) => {
-    const itemRef = doc(
-      getCollection(COLLECTIONS.menu, getUserPath("")),
-      id.toString()
-    );
-    const itemSnap = await getDoc(itemRef);
-    if (!itemSnap.exists()) return;
+                await uploadBytes(imageRef, blob);
+                downloadURL = await getDownloadURL(imageRef);
+            }
 
-    const itemData = itemSnap.data();
-    if (itemData.imagePath) {
-      const imageRef = storageRef(storage, itemData.imagePath);
-      try {
-        await deleteObject(imageRef);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    await deleteDoc(itemRef);
-    set((state) => ({
-      menu: state.menu.filter((item) => item.id !== id),
-    }));
-  },
+            const finalItem = {
+                ...item,
+                image: downloadURL,
+                imagePath: storagePath,
+            };
 
-  addOrder: async (item) => {
-    const { barmanEmail } = get();
-    try {
-      const docRef = doc(
-        getCollection(COLLECTIONS.orders, getUserPath(barmanEmail))
-      );
-      await setDoc(docRef, item);
-    } catch (e) {
-      console.error(e);
-    }
-  },
+            const docRef = doc(
+                getCollection(COLLECTIONS.menu, getUserPath(''))
+            );
+            await setDoc(docRef, finalItem);
 
-  removeOrder: async (id) => {
-    await deleteDoc(
-      doc(getCollection(COLLECTIONS.orders, getUserPath("")), id.toString())
-    );
-    set((state) => ({
-      orders: state.orders.filter((item) => item.id !== id),
-    }));
-  },
+            set((state) => ({
+                menu: [...state.menu, { ...finalItem, id: docRef.id }],
+            }));
+        } catch (error) {
+            console.error(error);
+        }
+    },
 
-  addStorageItem: async (item) => {
-    const docRef = doc(getCollection(COLLECTIONS.storage, getUserPath("")));
-    await setDoc(docRef, item);
-    set((state) => ({
-      storage: [...state.storage, { id: docRef.id, ...item }],
-    }));
-  },
+    clearAll: () => {
+        set({
+            rooms: [],
+            menu: [],
+            orders: [],
+            storage: [],
+            areRoomsLoaded: false,
+            isMenuLoaded: false,
+            isOrdersLoaded: false,
+            isStorageLoaded: false,
 
-  updateStorageItem: async (id, newData) => {
-    await updateDoc(
-      doc(getCollection(COLLECTIONS.storage, getUserPath("")), id.toString()),
-      newData
-    );
-    set((state) => ({
-      storage: state.storage.map((item) =>
-        item.id === id ? { ...item, ...newData } : item
-      ),
-    }));
-  },
+            guestUsername: '',
+            barmanEmail: '',
+        });
+    },
 
-  removeStorageItem: async (id) => {
-    await deleteDoc(
-      doc(getCollection(COLLECTIONS.storage, getUserPath("")), id.toString())
-    );
-    set((state) => ({
-      storage: state.storage.filter((item) => item.id !== id),
-    }));
-  },
+    updateMenuItem: async (id, newData) => {
+        try {
+            const itemRef = doc(
+                getCollection(COLLECTIONS.menu, getUserPath('')),
+                id.toString()
+            );
+            const itemSnap = await getDoc(itemRef);
+            if (!itemSnap.exists()) return;
+
+            const oldData = itemSnap.data();
+            let downloadURL = oldData.image;
+            let storagePath = oldData.imagePath;
+
+            if (newData.image && newData.image.startsWith('file://')) {
+                if (oldData.imagePath) {
+                    try {
+                        const oldImageRef = storageRef(
+                            storage,
+                            oldData.imagePath
+                        );
+                        await deleteObject(oldImageRef);
+                    } catch (err) {
+                        console.warn(
+                            'Не вдалося видалити старе зображення:',
+                            err
+                        );
+                    }
+                }
+
+                const response = await fetch(newData.image);
+                const blob = await response.blob();
+
+                const imageName = `${Date.now()}_${Math.random()
+                    .toString(36)
+                    .substring(7)}.jpg`;
+                storagePath = `${getUserPath('')}/menu_images/${imageName}`;
+                const imageRef = storageRef(storage, storagePath);
+
+                await uploadBytes(imageRef, blob);
+                downloadURL = await getDownloadURL(imageRef);
+            }
+
+            const finalData = {
+                ...newData,
+                image: downloadURL,
+                imagePath: storagePath,
+            };
+
+            await updateDoc(itemRef, finalData);
+            set((state) => ({
+                menu: state.menu.map((item) =>
+                    item.id === id ? { ...item, ...finalData } : item
+                ),
+            }));
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    removeMenuItem: async (id) => {
+        const itemRef = doc(
+            getCollection(COLLECTIONS.menu, getUserPath('')),
+            id.toString()
+        );
+        const itemSnap = await getDoc(itemRef);
+        if (!itemSnap.exists()) return;
+
+        const itemData = itemSnap.data();
+        if (itemData.imagePath) {
+            const imageRef = storageRef(storage, itemData.imagePath);
+            try {
+                await deleteObject(imageRef);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        await deleteDoc(itemRef);
+        set((state) => ({
+            menu: state.menu.filter((item) => item.id !== id),
+        }));
+    },
+
+    addOrder: async (item) => {
+        const { barmanEmail } = get();
+        try {
+            const docRef = doc(
+                getCollection(COLLECTIONS.orders, getUserPath(barmanEmail))
+            );
+            await setDoc(docRef, item);
+        } catch (e) {
+            console.error(e);
+        }
+    },
+
+    removeOrder: async (id) => {
+        await deleteDoc(
+            doc(
+                getCollection(COLLECTIONS.orders, getUserPath('')),
+                id.toString()
+            )
+        );
+        set((state) => ({
+            orders: state.orders.filter((item) => item.id !== id),
+        }));
+    },
+
+    addStorageItem: async (item) => {
+        const docRef = doc(getCollection(COLLECTIONS.storage, getUserPath('')));
+        await setDoc(docRef, item);
+        set((state) => ({
+            storage: [...state.storage, { id: docRef.id, ...item }],
+        }));
+    },
+
+    updateStorageItem: async (id, newData) => {
+        await updateDoc(
+            doc(
+                getCollection(COLLECTIONS.storage, getUserPath('')),
+                id.toString()
+            ),
+            newData
+        );
+        set((state) => ({
+            storage: state.storage.map((item) =>
+                item.id === id ? { ...item, ...newData } : item
+            ),
+        }));
+    },
+
+    removeStorageItem: async (id) => {
+        await deleteDoc(
+            doc(
+                getCollection(COLLECTIONS.storage, getUserPath('')),
+                id.toString()
+            )
+        );
+        set((state) => ({
+            storage: state.storage.filter((item) => item.id !== id),
+        }));
+    },
 }));
 
 export default useBarStore;
